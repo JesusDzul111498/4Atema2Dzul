d5151e89ab28bfcdb9c0eb646a3031ccfaaff0e6
Luis Coba <cobaluisenrique@gmail.com>
2019-02-23 18:39:55 -0600

    agregue algunos metodos

--- Proyecto/src/proyecto/UnidadDos/Main.java
+++ Proyecto/src/proyecto/UnidadDos/Main.java
@@ -3,12 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package proyecto;
+package proyecto.UnidadDos;
 
 /**
  *
- * @author Familiadc
+ * @author cobak
  */
-public class metodos {
+public class Main {
     
 }
